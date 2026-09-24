5641dcb1a8f5391e30c572b092925fe7a5d0839a
PegasusB <favivar229@ieszaidinvergeles.org>
2026-09-24 14:36:22 +0200

    ejercicio8.py

---
+++ Tipos de datos simples/ejercicio8.py
@@ -0,0 +1,8 @@
+n = int(input("Ponga un numero: "))
+m = int(input("Ponga otro numero: "))
+
+c = n // m
+r = n % m
+
+print(f"Este es el cociente de la division:  {c}")
+print(f"Este es el resto de la division:  {r}")
