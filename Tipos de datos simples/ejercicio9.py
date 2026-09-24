a619aedf734017822d664e045ccfcb9a0d93bdf5
PegasusB <favivar229@ieszaidinvergeles.org>
2026-09-24 14:42:24 +0200

    ejercicio9.py

---
+++ Tipos de datos simples/ejercicio9.py
@@ -0,0 +1,5 @@
+cantidadInversion = int(input("Ingrese la cantidad a invertir: "))
+interesAnual = int(input("Ingrese el interes anual: "))
+numAnos = int(input("Ingrese el numero de años: "))
+
+capitalObt= print()
